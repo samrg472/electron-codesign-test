53006b1975cbd70ab634c9e88a9ffc28ce1b17e6
samrg472 <samrg472@gmail.com>
2019-12-08 18:25:13 -0800

    Add tests

---
+++ tests/unit/electron.spec.js
@@ -0,0 +1,36 @@
+import { testWithSpectron } from 'vue-cli-plugin-electron-builder';
+
+jest.setTimeout(50000);
+
+let app;
+let stopServe;
+
+beforeAll(async () => {
+  const spectron = await testWithSpectron();
+  app = spectron.app;
+  stopServe = spectron.stopServe;
+});
+
+afterAll(async () => {
+  await stopServe();
+});
+
+beforeEach(async () => {
+  await app.restart();
+});
+
+test('Window Loads Properly', async () => {
+  const win = app.browserWindow;
+  const client = app.client;
+
+  // Window was created
+  expect(await client.getWindowCount()).toBe(1);
+  // It is not minimized
+  expect(await win.isMinimized()).toBe(false);
+  // Window is visible
+  expect(await win.isVisible()).toBe(true);
+  // Size is correct
+  const { width, height } = await win.getBounds();
+  expect(width).toBeGreaterThan(0);
+  expect(height).toBeGreaterThan(0);
+});
