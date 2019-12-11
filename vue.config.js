16472ea7d827348ee3149d528925f96742ecbf18
samrg472 <samrg472@gmail.com>
2019-12-11 11:36:39 -0800

    Force code signing

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,21 @@
+const FORCE_CODE_SIGNING = process.env.FORCE_CODE_SIGNING !== 'false';
+if (FORCE_CODE_SIGNING) {
+  console.log('Code signing is enforced!');
+} else {
+  console.log('[WARN] Code signing is not forced, builds may not be signed');
+}
+
 module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
         artifactName: '${name}-${version}-${os}-${arch}.${ext}',
         mac: {
+          forceCodeSigning: FORCE_CODE_SIGNING,
           identity: 'Electron CodeSign Test',
         },
         win: {
+          forceCodeSigning: FORCE_CODE_SIGNING,
           target: [{ target: 'nsis', arch: ['ia32', 'x64'] }],
         },
         linux: {
